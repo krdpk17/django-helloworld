effc6178916ccf27132f1a6ff2207fea4686133f
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-08 06:19:48 +0530

    /music shows link of each entry in this change

--- website/music/urls.py
+++ website/music/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import include,url
 from . import views 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    #mssic/712/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
 ]
