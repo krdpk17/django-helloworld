fd29425dd58ca3395e22861ed75471a81d6fab24
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-08 17:00:38 +0530

    working form to add album

--- website/music/models.py
+++ website/music/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Album(models.Model):
     artist = models.CharField(max_length=250)
@@ -6,6 +7,9 @@ class Album(models.Model):
     genre = models.CharField(max_length = 100)
     album_logo = models.CharField(max_length = 1000)
 
+    def get_absolute_url(self):
+        return reverse("music:detail", kwargs={"pk": self.pk})
+    
     def __str__(self):
         return self.album_title + '-' + self.artist
 
